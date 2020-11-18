3b5c993a83c1b1ebad60d5b82c24a8dee9c50c75
Ben Chussid <benchussid@Bens-MacBook-Pro.local>
2020-11-18 10:16:31 -0500

    authentication and basic crud functions

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,44 @@
-import './App.css';
-import Nav from './reactComponents/nav/Nav';
-import Home from './reactComponents/home/Home';
-import Footer from './reactComponents/footer/Footer';
-import CurrentShow from './reactComponents/currentshow/CurrentShow'
-import About from './About';
-import Artist from './Artist';
-import Reserve from './Reserve';
-import Admin from './reactComponents/forms/Admin'
-import AllShows from './reactComponents/allShows/AllShows'
-import Login from './reactComponents/forms/Login'
-
+import "./App.css";
+import Nav from "./reactComponents/nav/Nav";
+import Home from "./reactComponents/home/Home";
+import Footer from "./reactComponents/footer/Footer";
+import CurrentShow from "./reactComponents/currentshow/CurrentShow";
+import About from "./About";
+import Artist from "./Artist";
+import Reserve from "./Reserve";
+import Admin from "./reactComponents/forms/Admin";
+import AllShows from "./reactComponents/allShows/AllShows";
+import { AuthProvider } from "./reactComponents/forms/AuthContext";
+import Login from "./reactComponents/forms/Login";
+import Dashboard from "./reactComponents/forms/Dashboard";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
-   return (
-      <Router>
-         <div className="App">
-            <Nav />
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+        <AuthProvider>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+              <CurrentShow />
+            </Route>
+            <Route path="/allShows" component={AllShows} />
+            <Route path="/admin" component={Admin} />
+            <Route exact path="/admindashboard" component={Dashboard} />
+            <Route path="/login" component={Login} />
+            <Route path="/About" component={About} />
+            <Route path="/Artist" component={Artist} />
+            <Route path="/Reserve" component={Reserve} />
+          </Switch>
+        </AuthProvider>
 
-            <Switch>
-
-               <Route exact path="/">
-                  <Home />
-                  <CurrentShow />
-               </Route>
-               <Route path="./allShows" component={AllShows} />
-               <Route path="/admin" component={Admin} />
-               <Route path='/login' component={Login} />
-               <Route path="/About" component={About} />
-               <Route path="/Artist" component={Artist} />
-               <Route path="/Reserve" component={Reserve} />
-               
-            </Switch>
-
-            
-
-            <Footer />
-         </div>
-      </Router>
-   );
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- client/src/reactComponents/forms/Login.js
+++ client/src/reactComponents/forms/Login.js
@@ -1,50 +1,62 @@
-import React, {useRef, useState } from 'react'
-import { Form, Button, Card, Alert } from 'react-bootstrap'
-import { useAuth } from './AuthContext'
-// import { Link } from 'react-router-dom'
-
+import React, { useRef, useState } from "react";
+import { Form, Button, Card, Alert, Container } from "react-bootstrap";
+import { useAuth } from "./AuthContext";
 
 export default function Login() {
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  const { login } = useAuth()
-  const [error, setError] = useState
+  //grabs input from the form
+  const emailRef = useRef();
+  const passwordRef = useRef();
+  //gets values from login function (defined in AuthContext.js) & authenticates
+  const { login } = useAuth();
+
+  //error handling - starts free of errors
+  const [error, setError] = useState("");
+  //loading is set in state to disable button after it is clicked
+  //prevents multiple users from being created accidentally
+  const [loading, setLoading] = useState(false);
 
   async function handleSubmit(evt) {
-    setError('')
-    evt.preventDefault()
-    
+    evt.preventDefault();
+
     try {
-    await login(emailRef.current.value, passwordRef.current.value)
+      //initial state of form - free of errors & loading form (button is enabled)
+      setError("");
+      setLoading(true);
+      //waits for login to happen, otherwise, catch block is run and error is printed
+      await login(emailRef.current.value, passwordRef.current.value);
+      console.log("Login successful");
     } catch {
-      setError('Login Failed')
+      setError("Login Failed");
     }
+    setLoading(false);
   }
 
-
   return (
-    <>
-    <Card>
-      <Card.Body>
-        <h2 className='text-center mb-2'>Admin Login</h2>
-        {error && <Alert variant='danger'>{error}</Alert> }
-        <Form onSubmit={handleSubmit} >
-          <Form.Group id='email'>
-            <Form.Label>Email</Form.Label>
-            <Form.Control type='email' ref={emailRef} required />
-          </Form.Group>
-          <Form.Group id='password'>
-            <Form.Label>Password</Form.Label>
-            <Form.Control type='password' ref={passwordRef} required />
-          </Form.Group>
-          <Button type='submit'>Log In</Button>
-
-        </Form>
-      </Card.Body>
-
-    </Card>
-
-      
-    </>
-  )
+    <Container
+      className="d-flex align-items center justify-content-center mt-5"
+      style={{ minHeight: "80vh" }}
+    >
+      <div className="w-100" style={{ maxWidth: "420px" }}>
+        <Card>
+          <Card.Body>
+            <h2 className="text-center mb-2">Admin Login</h2>
+            {error && <Alert variant="danger">{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+              <Form.Group id="email">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" ref={emailRef} required />
+              </Form.Group>
+              <Form.Group id="password">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" ref={passwordRef} required />
+              </Form.Group>
+              <Button disabled={loading} className="w-100" type="submit">
+                Log In
+              </Button>
+            </Form>
+          </Card.Body>
+        </Card>
+      </div>
+    </Container>
+  );
 }
