585a32ff3355c76c239e67be986460f70d72d669
Ben Chussid <benchussid@Bens-MacBook-Pro.local>
2020-11-18 11:59:32 -0500

    switched auth back over to original firebase project

--- client/src/reactComponents/firebase/firebase.js
+++ client/src/reactComponents/firebase/firebase.js
@@ -7,13 +7,13 @@ import 'firebase/firestore'
 
 // configure firebase
 const app =  firebase.initializeApp({
-   apiKey: 'AIzaSyC-QSpHlepjOuyJHG8pp7sT6Gp8WVzDM9A',
-   authDomain: 'phantom-auth-test.firebaseapp.com',
-   databaseURL: 'https://phantom-auth-test.firebaseio.com',
-   projectId: 'phantom-auth-test',
-   storageBucket: 'phantom-auth-test.appspot.com',
-   messagingSenderId: '784040185097',
-   appId: '1:784040185097:web:71819b25d547914b6ad7d8'
+   apiKey: 'AIzaSyBi_A_sbAYFbzySxXCEWpHmwcA8U8GH8Ao',
+   authDomain: 'phantom-database-ee13a.firebaseapp.com',
+   databaseURL: 'https://phantom-database-ee13a.firebaseio.com',
+   projectId: 'phantom-database-ee13a',
+   storageBucket: 'phantom-database-ee13a.appspot.com',
+   messagingSenderId: '915188435781',
+   appId: '1:915188435781:web:f47068e4ba636fb18a4c2c'
 });
 
 
