e2d7efb2a05f2d10c174d4aa47ed6a5d946d0002
Andrea <andrea.vld.zrc@gmail.com>
2020-11-27 15:01:47 -0500

    changes to season.js

--- client/src/reactComponents/season/Season.js
+++ client/src/reactComponents/season/Season.js
@@ -2,48 +2,76 @@
 import React from "react";
 import "./Season.css";
 import Calendar from "../images/calendar.jpg";
+import { firestore } from "../firebase/firebase";
+import { useHistory} from 'react-router-dom'
+
+const collectAllIdsAndDocs = (doc) => {
+  return { id: doc.id, ...doc.data() };
+};
+
 
 // ------- Season function with calendar and show titles and dates
 function Season() {
+  
+let [allshows, setAllShows] = useState(null)
+const history = useHistory()
+
+async function seeAllShows(){
+
+  const showsRef = firestore.collection('shows')
+  const showSnapshot = await showsRef.where('status', '!=', 'proposal').get
+
+
+const allShowsArray = showSnapshot.docs.map(collectAllIdsAndDocs)
+if (!allShows) {
+  console.log('allShowsArray =', allShowsArray)
+  setAllShows(allShowsArray)
+}
+
+}
+
+
+
+
   return (
     <div className="season_container">
       <h1>Season Page!</h1>
       <div className="calendar_container">
-      <div className="calendar_season">
-        <img src={Calendar} />
+        <div className="calendar_season">
+          <img src={Calendar} />
+        </div>
+        <ul className="season_list">
+          <li>
+            <a href="#">Show Title!</a>20/20/2020
+          </li>
+          <li>
+            <a href="#">Show Title!</a> 20/20/2020
+          </li>
+          <li>
+            <a href="#">Show Title!</a> 20/20/2020
+          </li>
+
+          <li>
+            <a href="#">Show Title!</a> 20/20/2020
+          </li>
+          <li>
+            <a href="#">Show Title!</a> 20/20/2020
+          </li>
+          <li>
+            <a href="#">Show Title!</a> 20/20/2020
+          </li>
+
+          <li>
+            <a href="#">Show Title!</a> 20/20/2020
+          </li>
+          <li>
+            <a href="#">Show Title!</a> 20/20/2020
+          </li>
+          <li>
+            <a href="#">Show Title!</a> 20/20/2020
+          </li>
+        </ul>
       </div>
-      <ul className="season_list">
-        <li>
-          <a href="#">Show Title!</a>20/20/2020
-        </li>
-        <li>
-          <a href="#">Show Title!</a> 20/20/2020
-        </li>
-        <li>
-          <a href="#">Show Title!</a> 20/20/2020
-        </li>
-
-        <li>
-          <a href="#">Show Title!</a> 20/20/2020
-        </li>
-        <li>
-          <a href="#">Show Title!</a> 20/20/2020
-        </li>
-        <li>
-          <a href="#">Show Title!</a> 20/20/2020
-        </li>
-
-        <li>
-          <a href="#">Show Title!</a> 20/20/2020
-        </li>
-        <li>
-          <a href="#">Show Title!</a> 20/20/2020
-        </li>
-        <li>
-          <a href="#">Show Title!</a> 20/20/2020
-        </li>
-      </ul>
-    </div>
     </div>
   );
 }
